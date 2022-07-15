5dd301ffde7470f2fa0edc755cc303ddd90294b4
maximilian <Haximilian>
2022-07-15 10:34:58 -0700

    update HtmlNode data type and use higher level parser combinators

--- html-parser.hs
+++ html-parser.hs
@@ -1,39 +1,42 @@
-import Data.Map
+import Data.List
 import Data.Char
+
 import Text.ParserCombinators.ReadP
 
-data HtmlTag = Link | Text
+-- data HtmlNode = HtmlNode {
+--     content :: String,
+--     tag :: String
+-- } deriving Show
 
-data HtmlNode = HtmlNode {
-    content :: String,
-    tag :: String
-} deriving Show
+data HtmlNode =
+    HtmlContent String |
+    HtmlTag String [HtmlNode]
+    deriving Show
 
-contentParser :: ReadP String
-contentParser = many1 $ satisfy (\_ -> True)
+parseContent :: ReadP String
+parseContent = many get
 
 openTag :: ReadP String
 openTag = do
-    satisfy (== '<')
-    tag <- many1 $ satisfy isAlpha
-    satisfy (== '>')
+    char '<'
+    tag <- munch1 isAlpha
+    char '>'
     return tag
 
-closingTag :: String -> (ReadP String)
-closingTag tag = do
-    satisfy (== '<')
-    satisfy (== '/')
+closeTag :: String -> (ReadP String)
+closeTag tag = do
+    char '<'
+    char '/'
     string tag
-    satisfy (== '>')
+    char '>'
     return tag
-
 
 parseNode :: ReadP HtmlNode
 parseNode = do
     tag <- openTag
-    content <- contentParser
-    closingTag tag
-    return HtmlNode { content=content, tag=tag }
+    content <- parseContent 
+    closeTag tag
+    return $ HtmlTag tag [HtmlContent content]
 
 
 main :: IO ()
